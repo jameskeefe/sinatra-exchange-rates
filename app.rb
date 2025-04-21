494e41724885056469f57d698ae916678fc44e1d
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-21 14:53:46 +0000

    All done

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require "json"
 
 fxkey = ENV.fetch("EXCHANGE_RATE_KEY")
 
-currs = ["USD","EUR","GBP","JPY"]
+currs = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BRL','BSD','BTC','BTN','BWP','BYN','BYR','BZD','CAD','CDF','CHF','CLF','CLP','CNY','CNH','COP','CRC','CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GGP','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR','IQD','IRR','ISK','JEP','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LTL','LVL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRU','MUR','MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLE','SLL','SOS','SRD','STD','SVC','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH','UGX','USD','UYU','UZS','VEF','VES','VND','VUV','WST','XAF','XAG','XAU','XCD','XDR','XOF','XPF','YER','ZAR','ZMK','ZMW','ZWL']
 
 get("/") do
   @currencies = currs
@@ -15,26 +15,24 @@ get("/") do
   erb(:home)
 end
 
-get("/USD") do
-  @base = "USD"
+get("/:base") do
+  @base = params[:base]
   @currencies = currs
 
   erb(:select)
 end 
 
-get("/USD/EUR") do
+get("/:base/:quote") do
   fxkey = ENV.fetch("EXCHANGE_RATE_KEY")
 
-  # IRB for interactive console
-  
   # in the final version I'll need to only do the API call
-  @convert_from = "USD"
-  @convert_to = "EUR"
+  @base = params[:base]
+  @quote = params[:quote]
   
-  fx_url = "https://api.exchangerate.host/convert?from=#{@convert_from}&to=#{@convert_to}&amount=1&access_key=#{fxkey}"
-  
+  fx_url = "https://api.exchangerate.host/convert?from=#{@base}&to=#{@quote}&amount=1&access_key=#{fxkey}"
   data = HTTP.get(fx_url)
   data = JSON.parse(data)
+
   @rate = data['result'].to_f
 
   erb(:convert)
