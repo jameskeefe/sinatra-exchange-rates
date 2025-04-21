11ea5794cca7a745281a0deca013aad89123cfcd
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-21 13:42:55 +0000

    getting off train

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+
+require "http"
+require "json"
+
+fxkey = ENV.fetch("EXCHANGE_RATE_KEY")
 
 get("/") do
   "
@@ -7,3 +13,17 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+
+
+
+# in the final version I'll need to only do the API call
+convert_from = "USD"
+convert_to = "EUR"
+
+fx_url = "https://api.exchangerate.host/convert?from=#{convert_from}&to=#{convert_to}&amount=1&access_key=#{fxkey}"
+
+data = HTTP.get(fx_url)
+rate = data['result'].to_f
+
+pp rate
