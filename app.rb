28891329d651d0c8780870816badb0bde66e6313
jameskeefe <204756923+jameskeefe@users.noreply.github.com>
2025-04-21 15:03:25 +0000

    fixed currencies list

--- app.rb
+++ app.rb
@@ -7,18 +7,27 @@ require "json"
 
 fxkey = ENV.fetch("EXCHANGE_RATE_KEY")
 
-currs = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BRL','BSD','BTC','BTN','BWP','BYN','BYR','BZD','CAD','CDF','CHF','CLF','CLP','CNY','CNH','COP','CRC','CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GGP','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR','IQD','IRR','ISK','JEP','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LTL','LVL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRU','MUR','MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLE','SLL','SOS','SRD','STD','SVC','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH','UGX','USD','UYU','UZS','VEF','VES','VND','VUV','WST','XAF','XAG','XAU','XCD','XDR','XOF','XPF','YER','ZAR','ZMK','ZMW','ZWL']
-
 get("/") do
-  @currencies = currs
+  fx_url = "https://api.exchangerate.host/list?access_key=#{fxkey}"
+  data = HTTP.get(fx_url)
+  data = JSON.parse(data)
 
+
+  @currencies = data['currencies'].keys()
+  
   erb(:home)
+
 end
 
 get("/:base") do
-  @base = params[:base]
-  @currencies = currs
+  fx_url = "https://api.exchangerate.host/list?access_key=#{fxkey}"
+  data = HTTP.get(fx_url)
+  data = JSON.parse(data)
 
+  @currencies = data['currencies'].keys()
+
+  @base = params[:base]
+  
   erb(:select)
 end 
 
